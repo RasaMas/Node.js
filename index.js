9a73bac530428a3c92f1fa9eabac92fde2520127
RasaMas <144053645+RasaMas@users.noreply.github.com>
2023-12-12 22:23:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const casual = require('casual');   //same as import casual from 'casual';
+
+console.log(`${casual.country_code} ${casual.safe_color_name}`);
+console.log(`${casual.address}, ${casual.city}, ${casual.country}`);
+console.log(`${casual.first_name} ${casual.last_name}`);
